22738ddcd11ee384f1973977d0a37b5f323c44b8
SAmuel Anzora <00363823@uca.edu.sv>
2025-05-26 17:03:51 -0600

    :memo: add: añadi parcial2

--- Parcial2_00363823/app/src/main/java/com/parcial2/data/ProductRepository.kt
+++ Parcial2_00363823/app/src/main/java/com/parcial2/data/ProductRepository.kt
@@ -11,56 +11,56 @@ class ProductRepository {
                 name = "Laptop Lenovo",
                 category = "Tecnología",
                 price = 2999.99,
-                imageRes = R.drawable.images
+                imageRes = R.drawable.fdfs
             ),
             Product(
                 id = 2,
                 name = "Zapatillas Nike",
                 category = "Ropa",
                 price = 159.50,
-                imageRes = R.drawable.images
+                imageRes = R.drawable.nike
             ),
             Product(
                 id = 3,
                 name = "Smartphone Samsung",
                 category = "Tecnología",
                 price = 1299.00,
-                imageRes = R.drawable.images
+                imageRes = R.drawable.samsumd
             ),
             Product(
                 id = 4,
                 name = "Mochila Escolar",
                 category = "Accesorios",
                 price = 89.90,
-                imageRes = R.drawable.images
+                imageRes = R.drawable.mochula
             ),
             Product(
                 id = 5,
                 name = "Audífonos Bluetooth",
                 category = "Tecnología",
                 price = 199.99,
-                imageRes = R.drawable.images
+                imageRes = R.drawable.audifonos
             ),
             Product(
                 id = 6,
                 name = "Camisa Casual",
                 category = "Ropa",
                 price = 49.99,
-                imageRes = R.drawable.images
+                imageRes = R.drawable.camisa
             ),
             Product(
                 id = 7,
                 name = "Reloj Inteligente",
                 category = "Tecnología",
                 price = 899.00,
-                imageRes = R.drawable.images
+                imageRes = R.drawable.reloj
             ),
             Product(
                 id = 8,
                 name = "Botella Térmica",
                 category = "Accesorios",
                 price = 39.90,
-                imageRes = R.drawable.images
+                imageRes = R.drawable.botella
             )
         )
     }
